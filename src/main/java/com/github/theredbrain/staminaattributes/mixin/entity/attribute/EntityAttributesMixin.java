d574c36025ed97a7d3d5cd6be9c2c81755d16096
TheRedBrain <thetrueredbrain@gmail.com>
2024-05-24 11:00:13 +0200

    fixed attribute registration

--- src/main/java/com/github/theredbrain/staminaattributes/mixin/entity/attribute/EntityAttributesMixin.java
+++ src/main/java/com/github/theredbrain/staminaattributes/mixin/entity/attribute/EntityAttributesMixin.java
@@ -15,9 +15,9 @@ public class EntityAttributesMixin {
     }
 
     static {
-        StaminaAttributes.STAMINA_REGENERATION = register(StaminaAttributes.MOD_ID + "generic.stamina_regeneration", new ClampedEntityAttribute("attribute.name.generic.stamina_regeneration", 0.0F, -1024.0F, 1024.0F).setTracked(true));
-        StaminaAttributes.MAX_STAMINA = register(StaminaAttributes.MOD_ID + "generic.max_stamina", new ClampedEntityAttribute("attribute.name.generic.max_stamina", 0.0F, 0.0F, 1024.0F).setTracked(true));
-        StaminaAttributes.STAMINA_REGENERATION_DELAY_THRESHOLD = register(StaminaAttributes.MOD_ID + "generic.stamina_regeneration_delay_threshold", new ClampedEntityAttribute("attribute.name.generic.stamina_regeneration_delay_threshold", 60.0F, 0.0F, 1024.0F).setTracked(true));
-        StaminaAttributes.STAMINA_TICK_THRESHOLD = register(StaminaAttributes.MOD_ID + "generic.stamina_tick_threshold", new ClampedEntityAttribute("attribute.name.generic.stamina_tick_threshold", 20.0F, 0.0F, 1024.0F).setTracked(true));
+        StaminaAttributes.STAMINA_REGENERATION = register(StaminaAttributes.MOD_ID + ":generic.stamina_regeneration", new ClampedEntityAttribute("attribute.name.generic.stamina_regeneration", 0.0F, -1024.0F, 1024.0F).setTracked(true));
+        StaminaAttributes.MAX_STAMINA = register(StaminaAttributes.MOD_ID + ":generic.max_stamina", new ClampedEntityAttribute("attribute.name.generic.max_stamina", 0.0F, 0.0F, 1024.0F).setTracked(true));
+        StaminaAttributes.STAMINA_REGENERATION_DELAY_THRESHOLD = register(StaminaAttributes.MOD_ID + ":generic.stamina_regeneration_delay_threshold", new ClampedEntityAttribute("attribute.name.generic.stamina_regeneration_delay_threshold", 60.0F, 0.0F, 1024.0F).setTracked(true));
+        StaminaAttributes.STAMINA_TICK_THRESHOLD = register(StaminaAttributes.MOD_ID + ":generic.stamina_tick_threshold", new ClampedEntityAttribute("attribute.name.generic.stamina_tick_threshold", 20.0F, 0.0F, 1024.0F).setTracked(true));
     }
 }
