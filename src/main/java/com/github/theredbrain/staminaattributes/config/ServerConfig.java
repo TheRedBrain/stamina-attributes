de2b14c8e3df825809f7ba2591077e5ee5723630
TheRedBrain <thetrueredbrain@gmail.com>
2024-12-08 19:10:35 +0100

    bar overhaul
    -bumped Resource Bar API version
    -added automatic conversion from old .json5 config files
    -implemented changes required by Resource Bar API

--- src/main/java/com/github/theredbrain/staminaattributes/config/ServerConfig.java
+++ src/main/java/com/github/theredbrain/staminaattributes/config/ServerConfig.java
@@ -1,18 +1,23 @@
 package com.github.theredbrain.staminaattributes.config;
 
 import com.github.theredbrain.staminaattributes.StaminaAttributes;
-import me.fzzyhmstrs.fzzy_config.annotations.Comment;
+import me.fzzyhmstrs.fzzy_config.annotations.ConvertFrom;
 import me.fzzyhmstrs.fzzy_config.config.Config;
 import me.fzzyhmstrs.fzzy_config.config.ConfigSection;
 
+@ConvertFrom(fileName = "server.json5", folder = "staminaattributes")
 public class ServerConfig extends Config {
+
 	public ServerConfig() {
 		super(StaminaAttributes.identifier("server"));
 	}
+
 	public int item_use_cooldown_when_no_stamina = 20;
 	public boolean jumping_requires_stamina = true;
 	public boolean sprinting_requires_stamina = true;
+
 	public TickCostSection tickCosts = new TickCostSection();
+
 	public static class TickCostSection extends ConfigSection {
 		public float stamina_cost_sprinting = 0.05F;
 		public float stamina_cost_sneaking = 0.05F;
@@ -22,7 +27,9 @@ public class ServerConfig extends Config {
 		public float stamina_cost_walking_in_water = 0.05F;
 		public float stamina_cost_climbing = 0.05F;
 	}
+
 	public ActionCostSection actionCosts = new ActionCostSection();
+
 	public static class ActionCostSection extends ConfigSection {
 		public float stamina_cost_sprint_jumping = 1.0F;
 		public float stamina_cost_jumping = 1.0F;
