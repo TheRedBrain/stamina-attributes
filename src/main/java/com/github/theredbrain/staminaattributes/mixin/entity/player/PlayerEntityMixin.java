a95f3bd7fbf766d692f72f66bb62342d13a0fed0
TheRedBrain <thetrueredbrain@gmail.com>
2025-07-29 17:46:25 +0200

    reworked gamerule "naturalStaminaRegeneration"
    - now adds an EAM to players, when active
    - changed stamina regeneration attribute default to 0

--- src/main/java/com/github/theredbrain/staminaattributes/mixin/entity/player/PlayerEntityMixin.java
+++ src/main/java/com/github/theredbrain/staminaattributes/mixin/entity/player/PlayerEntityMixin.java
@@ -3,17 +3,31 @@ package com.github.theredbrain.staminaattributes.mixin.entity.player;
 import com.github.theredbrain.staminaattributes.StaminaAttributes;
 import com.github.theredbrain.staminaattributes.entity.StaminaUsingEntity;
 import com.github.theredbrain.staminaattributes.registry.GameRulesRegistry;
+import com.google.common.collect.HashMultimap;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.CustomModelDataComponent;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity implements StaminaUsingEntity {
@@ -24,6 +38,15 @@ public abstract class PlayerEntityMixin extends LivingEntity implements StaminaU
 
 	protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
 		super(entityType, world);
+	}
+
+	@Inject(method = "tick", at = @At("TAIL"))
+	public void staminaattributes$tick(CallbackInfo ci) {
+		if (this.getServer() != null && this.getServer().getGameRules().getBoolean(GameRulesRegistry.NATURAL_STAMINA_REGENERATION)) {
+			this.getAttributes().addTemporaryModifiers(getNaturalStaminaRegenerationModifier());
+		} else {
+			this.getAttributes().removeModifiers(getNaturalStaminaRegenerationModifier());
+		}
 	}
 
 	@Inject(method = "jump", at = @At("HEAD"), cancellable = true)
@@ -44,8 +67,10 @@ public abstract class PlayerEntityMixin extends LivingEntity implements StaminaU
 		}
 	}
 
-	@Override
-	public float staminaattributes$getRegeneratedStamina() {
-		return this.staminaattributes$getStaminaRegeneration() + (this.getServer() != null && this.getServer().getGameRules().getBoolean(GameRulesRegistry.NATURAL_STAMINA_REGENERATION) ? 1.0F : 0.0F);
+	@Unique
+	private HashMultimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> getNaturalStaminaRegenerationModifier() {
+		HashMultimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> hashMultimap = HashMultimap.create();
+		hashMultimap.put(StaminaAttributes.STAMINA_REGENERATION, new EntityAttributeModifier(StaminaAttributes.identifier("natural_stamina_regeneration_modifier"), 1.0, EntityAttributeModifier.Operation.ADD_VALUE));
+		return hashMultimap;
 	}
 }
