2f781e23433c0b5a71703fec20f5d4d6d918e674
TheRedBrain <thetrueredbrain@gmail.com>
2025-08-25 20:23:25 +0200

    game rule / attribute rework

--- src/main/java/com/github/theredbrain/staminaattributes/mixin/entity/player/PlayerEntityMixin.java
+++ src/main/java/com/github/theredbrain/staminaattributes/mixin/entity/player/PlayerEntityMixin.java
@@ -4,19 +4,14 @@ import com.github.theredbrain.staminaattributes.StaminaAttributes;
 import com.github.theredbrain.staminaattributes.entity.StaminaUsingEntity;
 import com.github.theredbrain.staminaattributes.registry.GameRulesRegistry;
 import com.google.common.collect.HashMultimap;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.CustomModelDataComponent;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registries;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
@@ -27,14 +22,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Optional;
-
 @Mixin(PlayerEntity.class)
-public abstract class PlayerEntityMixin extends LivingEntity implements StaminaUsingEntity {
+public abstract class PlayerEntityMixin extends LivingEntity {
 
 	@Shadow
 	@Final
 	private PlayerAbilities abilities;
+
+	@Shadow public abstract boolean isInCreativeMode();
 
 	protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
 		super(entityType, world);
@@ -42,16 +37,12 @@ public abstract class PlayerEntityMixin extends LivingEntity implements StaminaU
 
 	@Inject(method = "tick", at = @At("TAIL"))
 	public void staminaattributes$tick(CallbackInfo ci) {
-		if (this.getServer() != null && this.getWorld().getGameRules().getBoolean(GameRulesRegistry.NATURAL_STAMINA_REGENERATION)) {
-			this.getAttributes().addTemporaryModifiers(getNaturalStaminaRegenerationModifier());
-		} else {
-			this.getAttributes().removeModifiers(getNaturalStaminaRegenerationModifier());
-		}
+		this.getAttributes().addTemporaryModifiers(getNaturalStaminaModifiers(this.getWorld()));
 	}
 
 	@Inject(method = "jump", at = @At("HEAD"), cancellable = true)
 	public void staminaattributes$pre_jump(CallbackInfo ci) {
-		if (!this.abilities.invulnerable && StaminaAttributes.SERVER_CONFIG.jumping_requires_stamina && this.staminaattributes$getStamina() <= 0) {
+		if (!this.abilities.invulnerable && StaminaAttributes.SERVER_CONFIG.jumping_requires_stamina && ((StaminaUsingEntity) this).staminaattributes$getStamina() <= 0) {
 			ci.cancel();
 		}
 	}
@@ -60,17 +51,18 @@ public abstract class PlayerEntityMixin extends LivingEntity implements StaminaU
 	public void staminaattributes$post_jump(CallbackInfo ci) {
 		if (!this.abilities.invulnerable) {
 			if (this.isSprinting()) {
-				this.staminaattributes$addStamina(-StaminaAttributes.SERVER_CONFIG.actionCosts.stamina_cost_sprint_jumping);
+				((StaminaUsingEntity) this).staminaattributes$addStamina(-StaminaAttributes.SERVER_CONFIG.actionCosts.stamina_cost_sprint_jumping);
 			} else {
-				this.staminaattributes$addStamina(-StaminaAttributes.SERVER_CONFIG.actionCosts.stamina_cost_jumping);
+				((StaminaUsingEntity) this).staminaattributes$addStamina(-StaminaAttributes.SERVER_CONFIG.actionCosts.stamina_cost_jumping);
 			}
 		}
 	}
 
 	@Unique
-	private HashMultimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> getNaturalStaminaRegenerationModifier() {
+	private HashMultimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> getNaturalStaminaModifiers(World world) {
 		HashMultimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> hashMultimap = HashMultimap.create();
-		hashMultimap.put(StaminaAttributes.STAMINA_REGENERATION, new EntityAttributeModifier(StaminaAttributes.identifier("natural_stamina_regeneration_modifier"), 1.0, EntityAttributeModifier.Operation.ADD_VALUE));
+		hashMultimap.put(StaminaAttributes.STAMINA_REGENERATION, new EntityAttributeModifier(StaminaAttributes.identifier("natural_stamina_regeneration_modifier"), world.getGameRules().get(GameRulesRegistry.NATURAL_STAMINA_REGENERATION).get(), EntityAttributeModifier.Operation.ADD_VALUE));
+		hashMultimap.put(StaminaAttributes.MAX_STAMINA, new EntityAttributeModifier(StaminaAttributes.identifier("natural_maximum_stamina_modifier"), world.getGameRules().get(GameRulesRegistry.NATURAL_MAXIMUM_STAMINA).get(), EntityAttributeModifier.Operation.ADD_VALUE));
 		return hashMultimap;
 	}
 }
