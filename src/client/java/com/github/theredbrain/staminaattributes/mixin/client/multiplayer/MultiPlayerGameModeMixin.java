22cbd3a1f572d9c6c8eeafc64f6ae6deb09677f2
TheRedBrain <thetrueredbrain@gmail.com>
2026-04-12 11:34:16 +0200

    stop block breaking process when out of stamina

---
+++ src/client/java/com/github/theredbrain/staminaattributes/mixin/client/multiplayer/MultiPlayerGameModeMixin.java
@@ -0,0 +1,38 @@
+package com.github.theredbrain.staminaattributes.mixin.client.multiplayer;
+
+import com.github.theredbrain.staminaattributes.StaminaAttributes;
+import com.github.theredbrain.staminaattributes.config.ServerConfig;
+import com.github.theredbrain.staminaattributes.entity.StaminaUsingEntity;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(MultiPlayerGameMode.class)
+public class MultiPlayerGameModeMixin {
+
+	@Shadow
+	@Final
+	private Minecraft minecraft;
+
+	@Shadow
+	private BlockPos destroyBlockPos;
+
+	@WrapOperation(method = "continueDestroyBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;isAir()Z"))
+	public boolean staminaattributes$wrap_isAir(BlockState instance, Operation<Boolean> original) {
+		ServerConfig serverConfig = StaminaAttributes.SERVER_CONFIG;
+		boolean bl = (serverConfig.block_breaking_requires_stamina && this.minecraft.player != null && ((StaminaUsingEntity) this.minecraft.player).staminaattributes$getBlockBreakingActionStaminaCost() > 0 && ((StaminaUsingEntity) this.minecraft.player).staminaattributes$getStamina() <= 0);
+		if (bl && this.minecraft.level != null) {
+			this.minecraft.player.displayClientMessage(Component.translatable("hud.message.staminaTooLow"), true);
+			this.minecraft.level.destroyBlockProgress(this.minecraft.player.getId(), this.destroyBlockPos, -1);
+		}
+		return original.call(instance) || bl;
+	}
+}
