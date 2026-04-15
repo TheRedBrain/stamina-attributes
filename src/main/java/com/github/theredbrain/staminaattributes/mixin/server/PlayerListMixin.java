b0630f877730fabd300039185f80ed7249067588
TheRedBrain <thetrueredbrain@gmail.com>
2026-04-15 19:45:22 +0200

    fixed stamina not being full on first join

--- src/main/java/com/github/theredbrain/staminaattributes/mixin/server/PlayerListMixin.java
+++ src/main/java/com/github/theredbrain/staminaattributes/mixin/server/PlayerListMixin.java
@@ -5,6 +5,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.players.PlayerList;
+import net.minecraft.stats.Stats;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -14,6 +15,9 @@ public class PlayerListMixin {
 	@WrapOperation(method = "placeNewPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;initInventoryMenu()V"))
 	protected void staminaattributes$placeNewPlayer_wrap_initInventoryMenu(ServerPlayer instance, Operation<Void> original) {
 		((StaminaUsingEntity) instance).staminaattributes$setDelayStaminaTick(true);
+		if (instance.getStats().getValue(Stats.CUSTOM.get(Stats.LEAVE_GAME)) <= 0) {
+			((StaminaUsingEntity) instance).staminaattributes$setDelayMaxValueApplication(true);
+		}
 		original.call(instance);
 	}
 
